166ec4ec133386bdd4b4810b6dfc4409b4f72f40
Ayaz Zafar <ayaziqbalzafar@gmail.com>
2022-02-15 17:53:51 +0000

    Update HomepageFeatures.js

--- ayazwebsite/src/components/HomepageFeatures.js
+++ ayazwebsite/src/components/HomepageFeatures.js
@@ -5,7 +5,7 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('../../static/img/logo.svg').default,
+    Svg: require('../../static/img/education1.svg').default,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -15,7 +15,7 @@ const FeatureList = [
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('../../static/img/logo.svg').default,
+    Svg: require('../../static/img/education3.svg').default,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -25,7 +25,7 @@ const FeatureList = [
   },
   {
     title: 'Powered by React',
-    Svg: require('../../static/img/logo.svg').default,
+    Svg: require('../../static/img/education2.svg').default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
